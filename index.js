7911100a70e100a402e1429e312852a70ccf7840
ramha-ch <rimsha4ch@gmail.com>
2025-07-14 21:13:27 +0500

    Add Node.js app and deploy workflow

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from Node.js App!");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
